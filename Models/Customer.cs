eb32a33dfd009a8c29226a477a84b6bc35b75253
Haliam Pérez <haliampf@gmail.com>
2020-05-20 20:47:01 +0100

    Add Display Attribute to classes

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,21 @@ namespace MovieShareCore.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
+
+        [Display(Name = "Fecha de nacimiento")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Birthdate { get; set; }
+
+        [Display(Name = "Correo")]
         public string Mail { get; set; }
+
+        [Display(Name = "País")]
         public string Country { get; set; }
+
+        [Display(Name = "Administrador")]
         public bool Admin { get; set; }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,31 @@ namespace MovieShareCore.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Display(Name = "Título")]
         public string Title { get; set; }
+
+        [Display(Name = "Director")]
         public string Director { get; set; }
+
+        [Display(Name = "Año")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy}")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Duración")]
         public int Duration { get; set; }
+
+        [Display(Name = "Género")]
         public Genre Genre { get; set; }
+
+        [Display(Name = "Género")]
         public int GenreId { get; set; }
+
+        [Display(Name = "País")]
         public string Country { get; set; }
+
+        [Display(Name = "Presupuesto")]
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public int? Budget { get; set; }
     }
 }
