2e4766eca5e05d3745cf471ee2e3382d70372d33
Haliam Pérez <haliampf@gmail.com>
2020-06-09 13:27:24 +0100

    Implement dropdowns html in Movie Create view Add Jquery-ui Add  Live Sass

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var start = 1895;
+var currentyear = new Date().getFullYear();
+var options = "";
+for (var year = start; year <= currentyear; year++) {
+  options += "<option>" + year + "</option>";
+}
+document.getElementById("year").innerHTML = options;
