52717feb09c3eafdd33ff61b7dfb1515212d3779
Haliam Pérez <haliampf@gmail.com>
2020-07-07 13:25:17 +0100

    Populate Director in Movie Edit view Fixed GenreConfiguration

--- Data/Configurations/GenreConfiguration.cs
+++ Data/Configurations/GenreConfiguration.cs
@@ -13,26 +13,26 @@ namespace MovieShareCore.Data.Configurations
 
             builder.HasData
             (
-                new Customer { Id = 1, Name = "Action" },
-                new Customer { Id = 2, Name = "Adventure" },
-                new Customer { Id = 3, Name = "Animation" },
-                new Customer { Id = 4, Name = "Biography" },
-                new Customer { Id = 5, Name = "Comedy" },
-                new Customer { Id = 6, Name = "Crime" },
-                new Customer { Id = 7, Name = "Documentary" }, 
-                new Customer { Id = 8, Name = "Drama" },
-                new Customer { Id = 9, Name = "Fantasy" },
-                new Customer { Id = 10, Name = "FilmNoir" },
-                new Customer { Id = 11, Name = "History" },
-                new Customer { Id = 12, Name = "Horror" },
-                new Customer { Id = 13, Name = "Musical" },
-                new Customer { Id = 14, Name = "Mistery" },
-                new Customer { Id = 15, Name = "Romance" },
-                new Customer { Id = 16, Name = "SciFi" },
-                new Customer { Id = 17, Name = "Sport" },
-                new Customer { Id = 18, Name = "Thriller" },
-                new Customer { Id = 19, Name = "War" },
-                new Customer { Id = 20, Name = "Western" }
+                new Genre { Id = 1, Name = "Action" },
+                new Genre { Id = 2, Name = "Adventure" },
+                new Genre { Id = 3, Name = "Animation" },
+                new Genre { Id = 4, Name = "Biography" },
+                new Genre { Id = 5, Name = "Comedy" },
+                new Genre { Id = 6, Name = "Crime" },
+                new Genre { Id = 7, Name = "Documentary" }, 
+                new Genre { Id = 8, Name = "Drama" },
+                new Genre { Id = 9, Name = "Fantasy" },
+                new Genre { Id = 10, Name = "FilmNoir" },
+                new Genre { Id = 11, Name = "History" },
+                new Genre { Id = 12, Name = "Horror" },
+                new Genre { Id = 13, Name = "Musical" },
+                new Genre { Id = 14, Name = "Mistery" },
+                new Genre { Id = 15, Name = "Romance" },
+                new Genre { Id = 16, Name = "SciFi" },
+                new Genre { Id = 17, Name = "Sport" },
+                new Genre { Id = 18, Name = "Thriller" },
+                new Genre { Id = 19, Name = "War" },
+                new Genre { Id = 20, Name = "Western" }
             );
         }
     }
